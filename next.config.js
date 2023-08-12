733555e4f99b7396e7c6df6ebc0cc2ac4fd41542
Chakib Salah Eddine <61515408+Da3az@users.noreply.github.com>
2023-08-12 06:33:24 +0100

    attempt to fix canvas deploy error

--- next.config.js
+++ next.config.js
@@ -14,6 +14,16 @@ function getConfig(config) {
   return config;
 }
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
